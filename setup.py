4933bf2219f8b09a32b8f6692a89e3902f95ce9b
sallomon <sallomon@sally.com>
2017-02-02 22:03:41 +0100

    Modified templates

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Blog',
+    version='',
+    packages=['blog', 'blog.migrations', 'mysite'],
+    url='',
+    license='',
+    author='Sallomon',
+    author_email='',
+    description=''
+)
